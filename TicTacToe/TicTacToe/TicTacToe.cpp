e3daac3ce00ea66f646f189177bbfe04bcc137f5
MCBerberov19 <63785668+MCBerberov19@users.noreply.github.com>
2020-11-21 11:27:26 +0200

    Add inspectWin function

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+char cells[10] = { ' ','1','2','3','4','5','6','7','8','9' };
 void DrawBoardtype1() {
 	cout << "     |     |     " << endl;
 }
@@ -19,10 +19,44 @@ void DrawBoardtype4() {
 		else cout << "     |";
 	}
 }
+int inspectWin()
+{
+    if (cells[1] == cells[2] && cells[2] == cells[3])
+
+        return 1;
+    else if (cells[4] == cells[5] && cells[5] == cells[6])
+
+        return 1;
+    else if (cells[7] == cells[8] && cells[8] == cells[9])
+
+        return 1;
+    else if (cells[1] == cells[4] && cells[4] == cells[7])
+
+        return 1;
+    else if (cells[2] == cells[5] && cells[5] == cells[8])
+
+        return 1;
+    else if (cells[3] == cells[6] && cells[6] == cells[9])
+
+        return 1;
+    else if (cells[1] == cells[5] && cells[5] == cells[9])
+
+        return 1;
+    else if (cells[3] == cells[5] && cells[5] == cells[7])
+
+        return 1;
+    else if (cells[1] != '1' && cells[2] != '2' && cells[3] != '3'
+        && cells[4] != '4' && cells[5] != '5' && cells[6] != '6'
+        && cells[7] != '7' && cells[8] != '8' && cells[9] != '9')
+
+        return 0;
+    else
+        return 2;
+}
 
 int main()
 {
-	char cells[10] = { ' ','1','2','3','4','5','6','7','8','9' };
+	
 	DrawBoardtype1();
 	DrawBoardtype2(cells[1], cells[2], cells[3]);
 	DrawBoardtype3();
